2514060563208699a50eefe456aecb1b757c66ef
Michael J Baumli <michaeljbaumli@gmail.com>
2021-06-23 20:58:32 -0500

    Fixed a typo on DataFrame

--- 608-ch10.py
+++ 608-ch10.py
@@ -8,14 +8,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+f = 1
 c = 5/9 *(f-32)
 c = lambda f:5/9*(f-32)
+
 temps = [(f,c(f)) for f in range(0,101,10)]
 
-temps_df = pd.Dataframe(temps, columns=['Fahrenheit', 'Celsius'])
+
+temps_df = pd.DataFrame(temps, columns=['Fahrenheit', 'Celsius'])
 
 axes = temps_df.plot(x='Fahrenheit',y='Celsius', style='.-')
 
 y_label = axes.set_ylabel('Celsius')
+
+
+
 
 plt.show()
